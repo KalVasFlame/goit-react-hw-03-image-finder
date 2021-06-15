e5d8b56f4547bdba4f786217d31ce4bb2556b443
KalVasFlame <unnameddwar@gmail.com>
2021-06-15 21:33:45 +0300

    +

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+
+export default class Searchbar extends Component {
+  // static propTypes = {
+  //   prop: PropTypes
+  // }
+
+  state = {}
+
+  render() {
+    return (
+      <div>
+        <header className="Searchbar">
+          <form className="SearchForm">
+            <button type="submit" className="SearchForm-button">
+              <span className="SearchForm-button-label">Search</span>
+            </button>
+
+            <input
+              className="SearchForm-input"
+              type="text"
+              autocomplete="off"
+              autofocus
+              placeholder="Search images and photos"
+            />
+          </form>
+        </header>
+      </div>
+    )
+  }
+}
